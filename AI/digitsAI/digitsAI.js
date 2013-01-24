baa3c03e0d0431597d1d45dcdf8e53535cb63c17
BAUSSON Etienne <bausson.etienne@gmail.com>
2013-01-24 18:03:05 +0100

    Some rules added.

--- AI/digitsAI/digitsAI.js
+++ AI/digitsAI/digitsAI.js
@@ -1,51 +1,47 @@
 var digitsAI = function() {};
-var possibleValues;
-var tries;
-var postulates;
-
 
 digitsAI.prototype.play = function (game) {
   this.reset();
-  var verbose = !!game.verbose;
-  verbose && console.log();
-  verbose && console.log('Game summary :');
-  verbose && console.log(game);
-  verbose && console.log();
-  verbose && console.log();
+  this.verbose = !!game.verbose;
+  this.verbose && console.log();
+  this.verbose && console.log('Game summary :');
+  this.verbose && console.log(game);
+  this.verbose && console.log();
+  this.verbose && console.log();
   
-  this.generateBasePostulates();
-  while ( tries.length == 0 || ! tries[tries.length-1].won) {
-    var guess = this.generateGuess(game, verbose);
+  this.generateBasePostulates(game);
+  
+  while ( this.tries.length == 0 || ! this.tries[this.tries.length-1].won) {
+    var guess = this.generateGuess(game);
     var result = game.guess(guess);
-    console.log(result);
-    tries[tries.length] = result;
+    this.verbose && console.log(result);
+    this.tries[this.tries.length] = result;
   }
 }
 
-digitsAI.prototype.generateGuess = function (game, verbose) {
+digitsAI.prototype.generateGuess = function (game) {
   var guess = '';
-  verbose && console.log();
-  verbose && console.log();
-  verbose && console.log(' * Round n° ' + (tries.length+1));
-  verbose && console.log(' * Tries until now:');
-  verbose && console.log(tries);
-  verbose && console.log();
-  console.log(game);
+  this.verbose && console.log();
+  this.verbose && console.log();
+  this.verbose && console.log(' * Round n° ' + (this.tries.length+1));
+  this.verbose && console.log(' * Tries until now:');
+  this.verbose && console.log(this.tries);
+  this.verbose && console.log
   
-  if ( ! tries.length) {
+  if ( ! this.tries.length) {
     // first round
-    for (var i=0; i<game.length && i<possibleValues.length; i++){
-      guess += possibleValues[i];
+    for (var i=0; i<game.length && i<this.possibleValues.length; i++){
+      guess += this.possibleValues[i];
     }
     return guess;
   } else {
     // other rounds
-    this.calculatePostulates(tries[tries.length-1])
+    this.calculatePostulates(this.tries[this.tries.length-1])
     
-    
-    
-    guess = game.goal;
-    verbose && console.log(guess);
+    do {
+      guess = this.getRandomGuess(game.length);
+    } while ( ! this.isValidGuess(guess));
+    this.verbose && console.log('This round guess: ' + guess);
   }
   return guess;
 }
@@ -53,10 +49,64 @@ digitsAI.prototype.calculatePostulates = function(attempt){
   
 }
 
+digitsAI.prototype.generateBasePostulates = function(game){
+  // postulate 'only one value for a given colum'.
+  for (var column=1;column<=game.length;column++){
+    var postulateElems = [];
+    for (var j=0; j<this.possibleValues.length; j++){
+      postulateElems[j] = '(c' + column + '==' + this.possibleValues[j] + ')';
+    }
+    var postulate = postulateElems.join('^');
+    postulate = '(' + postulate + ' < 2)';
+    this.postulates[this.postulates.length] = postulate;
+  }
+  
+  
+  //postulate 'one value only once'.
+  for (var j=0; j<this.possibleValues.length; j++){
+    var postulateElems = [];
+    for (var column=1;column<=game.length;column++){
+      postulateElems[column-1] = '(c' + column + '==' + this.possibleValues[j] + ')';
+    }
+    var postulate = postulateElems.join('^');
+    postulate = '(' + postulate + ' < 2)';
+    this.postulates[this.postulates.length] = postulate;
+  }
+  
+  
+  
+  
+  
+  //TODO
+  //...
+  
+  
+  this.verbose && console.log(this.postulates);
+}
+
+
+digitsAI.prototype.getRandomGuess = function(gameSize) {
+  var result = '';
+  for (var i=0; i<gameSize; i++) {
+    var aValue = Math.floor(Math.random() * (this.possibleValues.length+1));
+    if (aValue >= this.possibleValues.length) {
+      aValue = this.possibleValues.length-1;
+    }
+    result += this.possibleValues[aValue];
+  }
+  return result;
+  //TODO
+}
+
+digitsAI.prototype.isValidGuess = function(guess) {
+  //TODO
+  return true;
+}
+
 digitsAI.prototype.reset = function() {
-  var possibleValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  tries = [];
-  postulates = [];
+  this.possibleValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  this.tries = [];
+  this.postulates = [];
 }
 
 exports = module.exports = new digitsAI;
